df40023c3b445ff65cb541f155ff2739eff31200
Snoeprol <mariovanrooij@hotmail.com>
2020-04-23 10:42:48 +0200

    new crystal cutter

---
+++ src/crystal_cutter.py
@@ -0,0 +1,7 @@
+from . import parser
+class Crystal:
+    def __init__(self, size, lattice_constant, kind):
+        self.size = size
+        self.lattice_constant = lattice_constant
+        self.kind = kind
+
